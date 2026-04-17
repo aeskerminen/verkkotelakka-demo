1de906c5fdc66d5a94714745587e4b9dde64a653
Artturi Kerminen <kerminenartturi@gmail.com>
2026-04-17 14:09:31 +0300

    chore: change layouts

--- lib/verkkotelakka_demo_web/components/layouts.ex
+++ lib/verkkotelakka_demo_web/components/layouts.ex
@@ -31,21 +31,23 @@ defmodule VerkkotelakkaDemoWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
+    <header class="navbar p-4 sm:px-4 lg:px-8 bg-slate-600 text-white">
+      <div class="flex-1 flex justify-between">
+        <h1 class="font-semibold">Verkkotelakka demo</h1>
+        <div class="flex gap-4">
+          <div class="flex-1 flex w-fit items-center gap-2">
+            <img src={~p"/images/logo.svg"} width="36" />
+            <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+          </div>
+        </div>
       </div>
     </header>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-fit space-y-4">
+    <main class="flex-1 flex justify-center items-center">
+      <div class="mx-auto w-2/3 space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
-
     """
   end
 end
